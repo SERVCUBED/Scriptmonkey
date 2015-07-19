782a469a10bb082ff9aaba727b4437160415c5f1
Ben Blain <mail@servc.eu>
2015-07-19 16:29:01 +0100

    Fix capitalisation in path

--- BHOUserScript/Unregister.NETv4.0.30319.bat
+++ BHOUserScript/Unregister.NETv4.0.30319.bat
@@ -3,10 +3,10 @@
  if /i "%PROCESSOR_ARCHITEW6432%"=="AMD64" GOTO AMD64
  GOTO ERR
  :AMD64
-  "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe" /unregister "%~dp0\ScriptMonkey.dll"
+  "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe" /unregister "%~dp0\Scriptmonkey.dll"
  GOTO EXEC
  :EXEC
-	"C:\Windows\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /unregister "%~dp0\ScriptMonkey.dll"
+	"C:\Windows\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /unregister "%~dp0\Scriptmonkey.dll"
  GOTO END
  :ERR
  @echo Unsupported architecture "%processor_architecture%"! Defaulting to x86.
