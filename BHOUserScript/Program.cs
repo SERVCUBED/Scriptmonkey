dbe2a7ea4d590a5d096e13010fc84a497297daa2
Ben Blain <mail@servc.eu>
2016-01-28 09:37:27 +0000

    Remove unused include

--- BHOUserScript/Program.cs
+++ BHOUserScript/Program.cs
@@ -15,7 +15,6 @@ using System.Runtime.InteropServices.Expando;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 
 /**
  *       ____        _     __                 __           
