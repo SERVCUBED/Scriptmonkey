44a717139c7bd9c0caed6f7295d3d2589166c6b3
Ben Blain <mail@servc.eu>
2016-05-02 17:01:50 +0100

    Set version to 1.7.1 and link v1.1.0

--- BHOUserScript/Properties/AssemblyInfo.cs
+++ BHOUserScript/Properties/AssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("A userscript manager for Internet Explorer")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SERVCUBED")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyleft SERV CUBED 2015 GPL License")]
+[assembly: AssemblyProduct("Scriptmonkey")]
+[assembly: AssemblyCopyright("Copyleft SERV CUBED 2016 GPL License")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.7.0.0")]
-[assembly: AssemblyFileVersion("1.7.0.0")]
+[assembly: AssemblyVersion("1.7.1.0")]
+[assembly: AssemblyFileVersion("1.7.1.0")]
